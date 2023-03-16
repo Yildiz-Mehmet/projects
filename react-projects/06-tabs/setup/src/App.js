2821aa9d5a1bd9b102fb8c683937dc802609415f
yıldız <mesel4461@gmail.com>
2023-03-16 17:11:52 +0300

    first commit

---
+++ react-projects/06-tabs/setup/src/App.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = "https://course-api.com/react-tabs-project";
+function App() {
+  <></>;
+}
+
+export default App;
